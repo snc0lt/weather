2e11eb221fe650916cbea9f0d23b433609164c32
snc0lt <xjinxer@gmail.com>
2020-10-29 09:20:55 -0500

    media queries

---
+++ src/components/AddButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './AddCity.scss'
+
+function AddButton() {
+  return (
+    <div className="plus_icon">
+    </div>
+  )
+}
+
+export default AddButton
--- src/components/AddCity.js
+++ src/components/AddCity.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import AddButton from './AddButton'
 import './AddCity.scss'
 function AddCity() {
   return (
-    <div className='add_city_form'>
-      <form className='add_city_input'>
-        <input
-          placeholder='add location'
-          style={{ borderRadius: '5px', marginTop: '30px', backgroundColor:'#cdcdd1' }} type="text" name="city" />
-      </form>
-    </div>
+    <>
+      <div className='add_city_form'>
+        <form className='add_city_input'>
+          <input
+            placeholder='add location'
+            style={{ borderRadius: '5px', marginTop: '30px', backgroundColor: '#cdcdd1' }} type="text" name="city" />
+        </form>
+      </div>
+      <AddButton />
+    </>
   )
 }
 
